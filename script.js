65ce801a3f4cd10c8939caeda61e3336827758f5
yashbhardwaj1877-web <yashbhardwaj1877@gmail.com>
2026-03-11 14:53:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+/*let age = 20;
+console.log("Age:", age);
+console.log("Type of age:", typeof age);
+
+let name = "Yash";
+console.log("Name:", name);
+console.log("Type of name:", typeof name);
+
+let isStudent = true;
+console.log("Is Student:", isStudent);
+console.log("Type of isStudent:", typeof isStudent);
+
+let x;
+console.log("Value of x:", x);
+console.log("Type of x:", typeof x);
+
+let y = null;
+console.log("Value of y:", y);
+console.log("Type of y:", typeof y);
+
+// symbol 
+const sym1 = Symbol(4);
+const sym2 = Symbol(4);
+
+console.log("Symbol 1:", sym1);
+console.log("Symbol 2:", sym2);
+
+
+if (sym1 === sym2) {
+    console.log("true");
+} else {
+    console.log("false"); 
+}*/
+
+
+// myfunction
+
+function myfunction(){
+    {
+        alert("hello world");
+    }
+              
+                                
+            
+
